a5449580f2f9a8a0ebf4a072b4832dadd03cac5e
christopher maul <maul@vpn-campus-204.csuchico.edu>
2017-10-30 19:41:53 -0700

    added a base flow for base.html

--- mysite/rater/views.py
+++ mysite/rater/views.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def default(request):
+    context= {
+    }
+    return render(request, 'base.html', context)
